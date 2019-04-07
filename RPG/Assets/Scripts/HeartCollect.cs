fd46fd0dbc976c17dc2725b28bbdb4e64794f456
CaptainMich <michele.salvo96@gmail.com>
2019-04-07 17:37:41 +0200

    Add new game RPG; add heart and sound

---
+++ RPG/Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+    public int RotateSpeed = 2;
+    public AudioSource CollectSound;
+    public GameObject Heart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotateSpeed, 0, Space.World);
+    }
+
+    void OnTriggerEnter()
+    {
+        CollectSound.Play();
+        this.Heart.SetActive(false);
+    }
+}
