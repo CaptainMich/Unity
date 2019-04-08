a9fd8f519775906dc24142231fc56e96f0580b99
CaptainMich <michele.salvo96@gmail.com>
2019-04-08 17:43:59 +0200

    Add raycast and quest display

---
+++ RPG/Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour
+{
+    public static float DistanceFromTarget; // declare distance we can access from any specific point
+    public float ToTarget; // declare internal variable to store the distance
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit; 
+        if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward),out Hit))
+        {
+            ToTarget = Hit.distance;
+            DistanceFromTarget = ToTarget;
+        }
+    }
+}
---
+++ RPG/Assets/Scripts/Quest/Quest001Take.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest001Take : MonoBehaviour
+{
+    public float Distance;
+    public GameObject ActionDisplay; // E key
+    public GameObject ActionText;
+    public GameObject UIQuest; // quest on screen readable in a full
+    public GameObject Player;
+    public GameObject NoticeCamera; // different camera to view the quest
+
+    // Update is called once per frame
+    void Update()
+    {
+        // constantly monitor the distance
+        Distance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        // check if we are gonna activate the trigger 
+        if (Distance <= 3)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        // behaviour of pressing E key 
+        if (Input.GetButtonDown("Action"))
+        {
+            if (Distance <= 3) // check if the player is in range 
+            {
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                UIQuest.SetActive(true);
+                NoticeCamera.SetActive(true);
+                Player.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
