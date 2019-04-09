16b4e5d90d21c7cb921eb1c8ca1e3253fb17ac92
CaptainMich <michele.salvo96@gmail.com>
2019-04-09 19:43:40 +0200

    Add UI Controls

---
+++ RPG/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // assign to each quest in the game a number 
+    public static int ActiveQuestNumber;
+    public int InternalQuestNumber;
+
+    void Update()
+    {
+        InternalQuestNumber = ActiveQuestNumber;
+    }
+}
